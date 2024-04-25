89e35cde2320582a9f73fafc97ff8b9554164477
“Echo” <echorw927@gmail.com>
2024-04-24 18:25:46 -0700

    a

--- scripts/characters/player/player.gd
+++ scripts/characters/player/player.gd
@@ -3,12 +3,15 @@ extends CharacterBody2D
 
 const SPEED = 150.0
 const JUMP_VELOCITY = -400.0
+const DASH_SPEED = 500
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var facingLeft : bool = false
 
 func _physics_process(delta):
+
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
@@ -20,6 +23,10 @@ func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
+
+	# Handles dashing
+	dash(direction)
+
 	if direction:
 		velocity.x = direction * SPEED
 		$AnimatedSprite2D.play("move")
@@ -40,3 +47,9 @@ func _physics_process(delta):
 	elif not is_on_floor() and velocity.y <= 0:
 		$AnimatedSprite2D.play("jump")
 		$AnimatedSprite2D.set_frame_and_progress(0, 0.0)
+
+
+func dash(direction):
+	if Input.is_action_just_pressed("player_dash"):
+		
+		velocity.x = direction * DASH_SPEED
