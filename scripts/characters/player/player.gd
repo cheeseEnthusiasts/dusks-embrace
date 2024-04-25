119eda575a1ea1f505f7b6ad8aa6577201aa3777
WanderingMonument <Dingospoodle1@gmail.com>
2024-04-24 19:18:19 -0700

    Added a platform, changed lights
    and changed lights

--- scripts/characters/player/player.gd
+++ scripts/characters/player/player.gd
@@ -1,31 +1,25 @@
 extends CharacterBody2D
 
 
-const SPEED = 150.0
+var SPEED = 150.0
 const JUMP_VELOCITY = -400.0
 const DASH_SPEED = 500
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
+
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var facingLeft : bool = false
+var sprinting : bool = false
 
 func _physics_process(delta):
 
-	
-	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
-	# Handle Jump.
+		
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
 
-	# Handles dashing
-	dash(direction)
 
 	if direction:
 		velocity.x = direction * SPEED
@@ -47,9 +41,18 @@ func _physics_process(delta):
 	elif not is_on_floor() and velocity.y <= 0:
 		$AnimatedSprite2D.play("jump")
 		$AnimatedSprite2D.set_frame_and_progress(0, 0.0)
+		
+	#sprinting stuff
+	dash(direction)
+	if sprinting:
+		SPEED = 300
+	elif !sprinting:
+		SPEED = 150
+	
 
 
 func dash(direction):
 	if Input.is_action_just_pressed("player_dash"):
-		
-		velocity.x = direction * DASH_SPEED
+		sprinting = true
+	if Input.is_action_just_released("player_dash"):
+		sprinting = false
