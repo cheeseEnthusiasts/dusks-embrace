000b8bd72977454d205424e59e017ba26bacc712
WanderingMonument <Dingospoodle1@gmail.com>
2024-04-27 11:12:30 -0700

    Coyote jumping, new lights

--- scripts/characters/player/player.gd
+++ scripts/characters/player/player.gd
@@ -5,22 +5,31 @@ var SPEED = 150.0
 const JUMP_VELOCITY = -400.0
 const DASH_SPEED = 500
 
-
+var can_jump : bool
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var facingLeft : bool = false
 var sprinting : bool = false
 
-func _physics_process(delta):
 
+
+
+
+func _physics_process(delta):
+	if !is_on_floor():
+		coyote_time()
+
+	#gravity
 	if not is_on_floor():
 		velocity.y += gravity * delta
-		
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	#jumping
+	if Input.is_action_just_pressed("ui_accept") and can_jump:
 		velocity.y = JUMP_VELOCITY
-
+	#checking if can jump
+	if is_on_floor():
+		can_jump = true
+	#basic movement
 	var direction = Input.get_axis("ui_left", "ui_right")
-
-
+	
 	if direction:
 		velocity.x = direction * SPEED
 		$AnimatedSprite2D.play("move")
@@ -50,10 +59,9 @@ func _physics_process(delta):
 		SPEED = 300
 	elif !sprinting:
 		SPEED = 150
-	
 
 #sprinmt
-func dash(direction):
+func dash(_direction):
 	if Input.is_action_just_pressed("player_dash"):
 		sprinting = true
 	if Input.is_action_just_released("player_dash"):
@@ -65,3 +73,6 @@ func kill():
 		position.x = 0
 		position.y = 0
 
+func coyote_time():
+	await get_tree().create_timer(0.25).timeout
+	can_jump = false
