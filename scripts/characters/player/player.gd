58a7f457b320820547c5c35b0f928da05bacb56f
WanderingMonument <Dingospoodle1@gmail.com>
2024-05-10 19:31:36 -0700

    huge changes
    added a lamp, changed the player lighting, can toggle the lamp by pressing q

--- scripts/characters/player/player.gd
+++ scripts/characters/player/player.gd
@@ -2,15 +2,15 @@ extends CharacterBody2D
 
 @export var inv = Inventory
 
-var SPEED = 150.0
-const JUMP_VELOCITY = -400.0
-const DASH_SPEED = 500
+var SPEED = 500
+var JUMP_VELOCITY = -500
+const DASH_SPEED = 750
 
 var can_jump : bool
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var facingLeft : bool = false
 var sprinting : bool = false
-
+var lantern : bool = false
 
 
 
@@ -40,9 +40,15 @@ func _physics_process(delta):
 	move_and_slide()
 	if Input.is_action_just_pressed("ui_left")and !facingLeft: 
 		$AnimatedSprite2D.flip_h = true
+		$lantern.move_local_x(-25)
+		$lantern/PointLight2D.scale.x = $lantern/PointLight2D.scale.x * -1
+		$lantern/PointLight2D.move_local_x(-202)
 		facingLeft = true
 	if Input.is_action_just_pressed("ui_right")and facingLeft: 
 		$AnimatedSprite2D.flip_h = false
+		$lantern.move_local_x(25)
+		$lantern/PointLight2D.move_local_x(202)
+		$lantern/PointLight2D.scale.x = $lantern/PointLight2D.scale.x * -1
 		facingLeft = false
 		
 	if not is_on_floor() and velocity.y >= 0:
@@ -57,13 +63,34 @@ func _physics_process(delta):
 	#sprinting stuff
 	dash(direction)
 	if sprinting:
-		SPEED = 300
+		SPEED = 750
 	elif !sprinting:
-		SPEED = 150
+		SPEED = 500
+		
+		
+	#lantern holding
+	if lantern:
+		$lantern.visible = true
+	
+	else: 
+		$lantern.visible = false
+		
+	if Input.is_action_just_pressed("lantern-toggle") and lantern and !sprinting:
+		lantern = false
+	elif Input.is_action_just_pressed("lantern-toggle") and !lantern and !sprinting:
+		lantern = true
+
+	if lantern and !sprinting:
+		SPEED = 500.0
+		JUMP_VELOCITY = -250
+	elif !lantern and !sprinting:
+		SPEED = 750.0
+		JUMP_VELOCITY = -500
+
 
 #sprinmt
 func dash(_direction):
-	if Input.is_action_just_pressed("player_dash"):
+	if Input.is_action_just_pressed("player_dash") and !lantern:
 		sprinting = true
 	if Input.is_action_just_released("player_dash"):
 		sprinting = false
@@ -76,6 +103,7 @@ func kill():
 
 func coyote_time():
 	await get_tree().create_timer(0.25).timeout
-	can_jump = false
+	if velocity.y < 0:
+		can_jump = false
 
 
